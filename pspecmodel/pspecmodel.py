1af81fb617a3f551f01e1ed204c712baeaae7792
Aaron Ewall-Wice <aaronew@berkeley.edu>
2020-07-18 00:07:57 -0700

    starting things off.

--- pspecmodel/pspecmodel.py
+++ pspecmodel/pspecmodel.py
@@ -1,8 +1,120 @@
-import uvpspec
+from hera_pspec import uvpspec
+from hera_pspec import grouping
 import numpy as np
 
-class pspecmodel():
+class PspecModel():
+    """Container for power spectrum measurements and models.
 
 
-  def __init__():
-  
+    This class keeps track of power-spectrum measurements
+     (and their associated covariances and window functions)
+     along with a theoretical model and calculations of the likelihoods
+     given this model that propertly account for the window functions.
+     For now, this container assumes Gaussian measurement errors and
+     thus only keeps track of covariances but this may change in the future.
+
+     This class also only considers additive nuisance models.
+    """
+
+  def __init__(self, ps_files, theoretical_model, bias_model,
+               bias_prior, kbin_centers, kbin_widths, little_h=True,
+               weight_by_cov=True, history='', run_check=True):
+      """Initializes PspecModel object.
+
+      Parameters
+      ----------
+      ps_files : list of uvpspec files
+        List of uvpspec files that constitute our power-spectrum measurements
+        or a single uvpspec object. Our current framework assumes that these
+        each power spectrum measurement (or spectral window) are statistically
+        independent.
+
+      theoretical_model : func(k, little_h, **params) -> delta_sq [mK^2]
+        a function that takes as its arguments a numpy vector of k-values (floats)
+        ,a bool (little_h), and any number of additional parameters and returns a vector of
+        floats with the same shape as the k-vector. little_h specifies whether k
+        units are in h/Mpc or 1/Mpc.
+
+      theoretical_prior : func(params) -> prob
+        a function that takes as its arguments a dictionary of theoretical parameters
+        and returns a prior probability for these parameters.
+
+      bias_model : func(k, little_h, **params) -> delta_sq [mK^2]
+        a function that takes in as its arguments a numpy vector of k-values (floats)
+        and a bool (little_h) and any additional number of theory params and returns
+        a vector of floats with the same shape as the k-vector.
+        The nuisance model should be defined in data space
+        and can be treaded as the bias term in
+        \hat{p} = W p_true + b
+        little_h specifies whether k units are in h/Mpc or 1/Mpc
+
+     bias_prior : func(params) -> prob
+        a function that takes as its arguments a dictionary of nuisance parameters
+        and returns a prior probability for these parameters.
+
+    k_bins : array-like floats
+        a list of floats specifying the centers of k-bins.
+
+     history : str
+        string with file history.
+
+      Attributes
+      -------
+        measurements : UVPspec
+            uvpspec object generated from list of uvpspec files. Each
+            spectral window is assumed to be statistically independent.
+
+        theory_func : func(k, params, little_h, ps_units) -> p(k)
+            function provided in theoretical_model arg.
+
+        theoretical_prior : func(params) -> prob
+            function provided in theoretical_prior
+
+        bias_model : func(k, params, little_h, ps_units) -> p(k)
+            function provided in nuisance_model
+
+        bias_prior : func(params) -> prob
+            functin provided in nuisance_prior
+
+        history : str
+            string provided in history arg.
+      """
+      if isinstance(ps_files, string):
+          ps_files = [ps_files]
+      # what assumptions are we making about the power spectra?
+      # are they spherically averaged?
+      if not isinstance(ps_files, list):
+          raise ValueError("ps_files must be a string or a list of strings.")
+      uvp_in = UVPspec(ps_file_list)
+      # spherically average all power spectrum measurements using inverse covariance
+      # weighting.
+      self.measurements = grouping.spherical_average(uvp_in, kbin_centers, kbin_widths,
+                                                     time_average=True, weight_by_cov=weight_by_cov,
+                                                     add_to_history='spherical average with time averaging.',
+                                                     little_h=little_h, run_check=run_check)
+      self.theoretical_model = theoretical_model
+      self.nuisance_model = nuisance_model
+      self.nuisance_model = nuisance_prior
+      self.history = history
+      self.little_h = little_h
+
+  def windowed_theoretical_ps(theory_params, spw):
+      """Calculate theoretical power spectrum with data window function applied.
+
+      Parameters
+      ----------
+        theory_params : dict
+            dictionary of theoretical parameters.
+        spectral_window : int
+            number of spectral window to generate windowed theoretical ps.
+        little_h : bool, optional
+            if true, use little_h units (e.g. h^-1 Mpc)
+      Returns
+      -------
+        A vector of floats, p_w = W p_m
+        where p_m is a theoretical model power spectrum, W is the window function applied
+        to that model.
+      """
+      k_values = np.sqrt(self.measurements.get_kperps(spw, self.little_h) ** 2. + self.measurements.get_kparas(spw, self.little_h) ** 2.)
+      true_ps = self.theoretical_model(k_values, little_h, **theory_params)
+      windows_ps = self.measurements.get_window_function(spw, )
