7d30b51fdb2ed1873ced203444bd620cc845ffd4
Steven Murray <steven.g.murray@asu.edu>
2022-01-18 16:24:36 -0700

    style: add further checks on style

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,10 @@
 """Very basic tests of the infrastructure."""
-from pspec_likelihood import PSpecLikelihood
 
 
 def test_null():
     """Really just a placeholder so that the import is triggered."""
+    from pspec_likelihood import PSpecLikelihood
+
+    assert callable(PSpecLikelihood)
+
     assert True
