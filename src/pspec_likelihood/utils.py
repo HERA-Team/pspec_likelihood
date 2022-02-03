ad7d3a139f5e5ee44806552aa0235db226b7236f
Steven Murray <steven.g.murray@asu.edu>
2022-02-03 15:26:35 -0700

    refactor: add typing to utils

--- src/pspec_likelihood/utils.py
+++ src/pspec_likelihood/utils.py
@@ -1,16 +1,13 @@
 """Miscellaneous utility functions."""
 from pathlib import Path
+from typing import Any, Sequence
 
 
-def listify(x):
+def listify(x: Any) -> list:
     """If x is not a list, put it into one."""
-    if not isinstance(x, (list, tuple, set)):
-        return [x]
-    else:
-        return list(x)
+    return [x] if not isinstance(x, (list, tuple, set)) else list(x)
 
 
-def make_list_of_paths(x):
+def make_list_of_paths(x: str | Path | Sequence[str | Path]) -> list[Path]:
     """Convert a list of strings (or single string) into a list of Path objects."""
-    x = listify(x)
-    return [Path(xx) for xx in x]
+    return [Path(xx) for xx in listify(x)]
