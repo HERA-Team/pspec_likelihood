f38d409740f044765978c01180d7ced1b406ead2
Stefan Heimersheim <heimersheim@ast.cam.ac.uk>
2022-06-09 12:49:41 +0100

    rename powerlaw_eor

--- tests/test_likelihoods.py
+++ tests/test_likelihoods.py
@@ -11,7 +11,7 @@ from pspec_likelihood import (DataModelInterface,
                               MarginalizedLinearPositiveSystematics)
 
 
-def dummy_theory_model(z, k, params):
+def powerlaw_eor(z, k, params):
     return params[0] * k ** params[1] * un.mK**2
 
 
@@ -27,7 +27,7 @@ def test_like():
     dmi1 = DataModelInterface.from_uvpspec(
         uvp1,
         band_index=1,
-        theory_model=dummy_theory_model,
+        theory_model=powerlaw_eor,
         sys_model=dummy_sys_model,
         theory_uses_spherical_k=True,
         kpar_bins_theory=np.linspace(0.1, 1, 40) / un.Mpc,
